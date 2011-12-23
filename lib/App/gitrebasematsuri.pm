a4cf3e11756c253a1ee61ddc8b6a2092a3dcb5b0
issm <issmxx@gmail.com>
2011-12-23 15:34:51 +0900

    initial commit

---
+++ lib/App/gitrebasematsuri.pm
@@ -0,0 +1,32 @@
+package App::gitrebasematsuri;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::gitrebasematsuri -
+
+=head1 SYNOPSIS
+
+  use App::gitrebasematsuri;
+
+=head1 DESCRIPTION
+
+App::gitrebasematsuri is
+
+=head1 AUTHOR
+
+issm E<lt>issmxx@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
